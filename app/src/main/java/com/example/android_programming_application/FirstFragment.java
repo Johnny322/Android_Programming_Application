1e71f004b805843ca25c11706f2df73a44a26483
Kristian1997 <32383060+Kristian1997@users.noreply.github.com>
2020-05-27 14:40:06 +0200

    Fixed stuff

--- app/src/main/java/com/example/android_programming_application/FirstFragment.java
+++ app/src/main/java/com/example/android_programming_application/FirstFragment.java
@@ -4,59 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
-<<<<<<< Updated upstream
+import java.util.List;
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-=======
+public class FirstFragment extends Fragment {
+
     private ListView listView;
     private int page = 1;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-        listView = view.findViewById(R.id.listView);
-        MovieRepository.getInstance().getMovies(page, CatagoryEnum.POPULAR, new OnGetMoviesCallback() {
-            @Override
-            public void onSuccess(int page, List<Movie> movies) {
-                MovieAdaptor movieAdaptor = new MovieAdaptor(movies);
-                listView.setAdapter(movieAdaptor);
-            }
+        public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle
+        savedInstanceState){
+            View view = inflater.inflate(R.layout.fragment_first, container, false);
+            listView = view.findViewById(R.id.listView);
+            MovieRepository.getInstance().getMovies(page, CatagoryEnum.POPULAR, new OnGetMoviesCallback() {
+                @Override
+                public void onSuccess(int page, List<Movie> movies) {
+                    MovieAdaptor movieAdaptor = new MovieAdaptor(movies);
+                    listView.setAdapter(movieAdaptor);
+                }
 
-            @Override
-            public void onError() {
+                @Override
+                public void onError() {
 
-            }
-        });
->>>>>>> Stashed changes
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+                }
+            });
+
+            return inflater.inflate(R.layout.fragment_first, container, false);
+        }
+        public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+        }
     }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-<<<<<<< Updated upstream
-        });
-    }
-=======
-        });*/
-    }
-
->>>>>>> Stashed changes
-}
